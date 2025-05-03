e315b4b078f83b47e238a8d5bb6f6e07d045fd5f
Samarth Sheth <samarth.sheth.29@gmail.com>
2025-05-03 17:32:36 +0530

    Android setup

--- android/src/main/java/com/apkinstaller/ApkInstallerModule.kt
+++ android/src/main/java/com/apkinstaller/ApkInstallerModule.kt
@@ -1,9 +1,14 @@
 package com.apkinstaller
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import androidx.core.content.FileProvider
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import java.io.File
 
 class ApkInstallerModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -12,11 +17,28 @@ class ApkInstallerModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun installApk(filePath: String) {
+    val context = reactApplicationContext
+    val file = File(filePath)
+
+    if (!file.exists()) {
+      println("APK file not found at path: $filePath")
+      return
+    }
+
+    val intent = Intent(Intent.ACTION_VIEW)
+    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+    val apkUri: Uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+      intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+      FileProvider.getUriForFile(context, "${context.packageName}.provider", file)
+    } else {
+      Uri.fromFile(file)
+    }
+
+    intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
+    context.startActivity(intent)
   }
 
   companion object {
